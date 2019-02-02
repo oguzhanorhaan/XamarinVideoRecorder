3228b617d54f5b97d23729c5b2453f802b478baf
oguzhanorhan <ozy.orhan@gmail.com>
2019-02-02 19:00:19 +0300

    Xamarin.Forms Custom Video Recorder
    This is a custom video recorder which is implemented for both platforms iOS and Android.

---
+++ XamarinVideoRecorder/XamarinVideoRecorder/CameraPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinVideoRecorder
+{
+    //NOTE: There are messaging center implementations in CustomRenderers for your usages, you can usage data in this class by implementing your messaging center in here 
+
+	public class CameraPage : ContentPage
+	{
+		public CameraPage ()
+		{
+            BackgroundColor = Color.Black;
+            NavigationPage.SetHasNavigationBar(this, false);
+           
+        }
+	}
+}
---
+++ XamarinVideoRecorder/XamarinVideoRecorder/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinVideoRecorder
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new CameraPage());
+        }
+    }
+}
